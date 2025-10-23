32e970975a7de905b103370b90db952f9fbd889b
RuyOllivere <henriquerubine@gmail.com>
2025-10-23 09:59:55 -0300

    verify-login-function

--- src/config/config.php
+++ src/config/config.php
@@ -18,4 +18,23 @@ require_once '../utils/Session.php';
 // Initialize secure session
 Session::start();
 
+// Conecition pdo errors
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbName=$dbName;charset=$charset;", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ]);
+} catch(PDOException $e){
+    error_log('Connection error: ' . $e->getMessage());
+    die('System error. Try again later.');
+}
+
+// Verifing if user is logged
+
+function isLoggedIn() {
+    return isset($_SESSION['user_id']) && !empty($_SESSION['user_id']);
+}
+
 ?>
