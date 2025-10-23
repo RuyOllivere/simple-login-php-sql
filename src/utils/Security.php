11a79bfa1ed10bcb9393d542a5da66aaa0f72a4a
RuyOllivere <henriquerubine@gmail.com>
2025-10-23 11:31:40 -0300

    feat: redirect

--- src/utils/Security.php
+++ src/utils/Security.php
@@ -28,6 +28,37 @@ class Security{
     public static function validateName($name) {
         return !empty($name) && strlen($name) >= 2 && strlen($name) <= 100;
     }
+
+    public static function logAccess($pdo, $user_id, $action, $success){
+        try{
+            $sql = "INSERT INTO logs_acesso (usuario_id, ip_address, user_agent, acao, sucesso) 
+            VALUES (?, ?, ?, ?, ?)";
+
+            $stmt = $pdo->prepare($sql);
+
+            $stmt->execute([
+                $user_id,
+                $_SERVER['REMOTE_ADDR'],
+                $_SERVER['HTTP_USER_AGENT'] ?? 'Unknown',
+                $action,
+                $success ? 1 : 0
+            ]);
+            return true;
+        } catch(PDOException $e){
+            error_log("Error to registrate: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public static function redirect($url, $message = null){
+        if($message){
+            $_SESSION['flash_message'] = $message;
+        }
+        header('Location:' . $url);
+
+        exit();
+    }
+
 }
 
 ?>
