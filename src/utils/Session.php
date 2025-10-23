a007ed410a4af523a6d7035aab6eb691b2eb8c1d
RuyOllivere <henriquerubine@gmail.com>
2025-10-23 11:50:05 -0300

    feat: Session starting

--- src/utils/Session.php
+++ src/utils/Session.php
@@ -2,7 +2,19 @@
 
 class Session {
     public static function start(){
-        
+        if(session_status() == PHP_SESSION_NONE){
+            session_set_cookie_params([
+                'lifetime' => 0,
+                'path' => '/',
+                'domain' => '',
+                'secure' => true,
+                'httponly' => true,
+                'samesite' => 'Strict'
+            ]);
+
+            session_start();
+
+        }
     }
 }
 
