77cfee5732d33cd1290d2275fc5dd79d5f85984c
RuyOllivere <henriquerubine@gmail.com>
2025-10-23 11:40:11 -0300

    fix: adding comments to clarify code functionality (Security.php)

--- src/utils/Security.php
+++ src/utils/Security.php
@@ -1,10 +1,13 @@
 <?php
 
 class Security{
+
+    // remove danger inputs and prevent XSS
     public static function sanitizeInput($data) {
         return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
     }
 
+    // Generate Token
     public static function generateCSFRToken(){
         if (empty($_SESSION['csrf_token'])){
             $_SESSION['csrf_token'] = bin2hex(random_bytes(64));
@@ -13,22 +16,27 @@ class Security{
         return $_SESSION['csrf_token'];
     }
 
+    // Verify if the token is valid
     public static function verifyCSRFToken($token){
         return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
     }
 
+    // Validate email
     public static function validateEmail($email){
         return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
+    // Validate if the password is "safe"
     public static function validatePassword($password){
         return strlen($password) >= 8;
     }
 
+    // Validate the lenght min and max of a name
     public static function validateName($name) {
         return !empty($name) && strlen($name) >= 2 && strlen($name) <= 100;
     }
 
+    // Generate the log access of a user for aud
     public static function logAccess($pdo, $user_id, $action, $success){
         try{
             $sql = "INSERT INTO logs_acesso (usuario_id, ip_address, user_agent, acao, sucesso) 
