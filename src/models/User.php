d9569259fddd4143e9a826e7faac0aae61c578b4
RuyOllivere <ruylivinginadream@duck.com>
2025-10-28 09:05:48 -0300

    function: findByEmail method in User Repositoy class & findById method

--- src/models/User.php
+++ src/models/User.php
@@ -2,7 +2,34 @@
 
 class User{
     private $pdo;
-    
+
+    public function __construct($pdo)
+    {
+        $this -> pdo = $pdo;
+    }
+
+    public function findByEmail($email) {
+        $sql = "SELECT id, nome, email, senha_hash, ativo, data_cadastro, ultimo_login, FROM usuarios WHERE email =?";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$email]);
+
+        return $stmt->fetch();
+    }
+
+    public function findById($user_id){
+
+        $sql = "SELECT id, nome, email, senha_hash, ativo, data_cadastro, ultimo_login, FROM usuarios WHERE id = ? AND ativo = 1";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$user_id]);
+
+        return $stmt->fetch();
+
+        
+
+    }
+
 }
 
 ?>
